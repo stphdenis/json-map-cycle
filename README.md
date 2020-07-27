c2f91b69f18fa6a55dfcf8db8f837dfeb365b88d
stphdenis <stph.denis@gmail.com>
2020-07-27 21:44:40 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# json-map-cycle
+
+JSON library with parse and stringify for `Map`, `Set`, `Date` and `cycle`.
+
+Use the default `JSON` package an has exactly the same signature than `JSON` from `Node`.
+
+`replacer` and `reviver` can still be used.
+
+`decycle` and `recycle` from package `cycle` are both optional.
+
+Can be used as static or as an object with different parameters.
+
+You can use `Json.setOptions({...})` when starting your app before using static form.
+
+## stringify
+
+Stringify `Map` to
+
+- `{ anyKey: { "$map": [...] } }`
+- `[...]`
+
+Stringify `Set` to
+
+- { anyKey: { "$set": [...] } }`
+- `[...]`
+
+Stringify `Date` for `JSON` or `UTC` format to
+
+- `{ anyKey: { "$date": "..." } }`
+- `"..."`
+
+Can `decycle` the parsed object with `$ref` keys (off by default).
+
+## parse
+
+Parse a `new Map()` from 
+
+- `{ anyKey: { "$map": [...] } }`
+- `{ knownMapKeys: [...] }`
+- `{ regexMapKeys: [...] }`
+
+Parse a `new Set()` from
+
+- `{ anyKey: { "$set": [...] } }`
+- `{ knownSetKeys: [...] }`
+- `{ regexSetKeys: [...] }`
+
+Parse a `new Date()` from
+
+- `{ anyKey: { "$date": "..." } }`
+- `{ knownDateKeys: "..." }`
+- `{ regexDateKeys: "..." }`
+
+Can `recycle` the parsed object from `$ref` keys (on by default).
+
+## Examples
+
+With the static form :
+
+```ts
+import { Json } from 'json-map-cycle'
+
+// replacer to decycle "of" keys to "$ref" a known type
+function replacer(this: any, key: string, value: any): any {
+  if(key && key === 'of') {
+    return { $ref: `$[\"types\"][\"${value.name}\"]` }
+  }
+}
+const jsonApiSchema = Json.stringify(apiSchema, replacer, 2)
+```
+
+With object form :
+
+```js
+import { Json } from 'json-map-cycle'
+
+const json = new Json({
+  ...opions...
+})
+const jsonApiSchema = json.stringify(apiSchema, null, 2)
+```
---
+++ dist/Json.js
@@ -0,0 +1,190 @@
+"use strict";
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, privateMap, value) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to set private field on non-instance");
+    }
+    privateMap.set(receiver, value);
+    return value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, privateMap) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to get private field on non-instance");
+    }
+    return privateMap.get(receiver);
+};
+var _options;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Json = void 0;
+const cycle = require("cycle");
+const defaultJsonOptions = {
+    parse: {
+        dateSchema: ['$date', 'JSON&UTC'],
+        dateStringKeys: null,
+        dateRegexKeys: null,
+        dateType: "Date",
+        setSchema: '$set',
+        setStringKeys: null,
+        setRegexKeys: null,
+        mapSchema: '$map',
+        mapStringKeys: null,
+        mapRegexKeys: null,
+        retrocycle: true,
+    },
+    stringify: {
+        dateSchema: false,
+        dateFormat: 'JSON',
+        setSchema: '$set',
+        mapSchema: '$map',
+        decycle: false,
+    },
+};
+class Json {
+    constructor(options) {
+        _options.set(this, void 0);
+        __classPrivateFieldSet(this, _options, defaultJsonOptions);
+        if (options) {
+            this.setOptions(options);
+        }
+    }
+    static parse(text, reviver) {
+        return this._json.parse(text, reviver);
+    }
+    static setOptions(options) {
+        this._json.setOptions(options);
+    }
+    static stringify(value, replacer, space) {
+        return this._json.stringify(value, replacer, space);
+    }
+    setOptions(options) {
+        Object.assign(__classPrivateFieldGet(this, _options).parse, options.parse);
+        Object.assign(__classPrivateFieldGet(this, _options).stringify, options.stringify);
+    }
+    stringify(value, replacer, space) {
+        if (__classPrivateFieldGet(this, _options).stringify.decycle) {
+            return JSON.stringify(cycle.decycle(value), this.replacer(replacer), space);
+        }
+        else {
+            return JSON.stringify(value, this.replacer(replacer), space);
+        }
+    }
+    replacer(replace) {
+        const that = this;
+        /**
+         * Eliminate recursion referencing the types
+         */
+        function replacer(key, value) {
+            const options = __classPrivateFieldGet(that, _options).stringify;
+            if (value instanceof Set) {
+                if (options.setSchema === '$set') {
+                    return { $set: [...value] };
+                }
+                else { // options.setSchema === 'array'
+                    return [...value];
+                }
+            }
+            if (value instanceof Map) {
+                if (options.mapSchema === '$map') {
+                    return { $map: [...value] };
+                }
+                else { // options.mapSchema === 'array'
+                    return [...value];
+                }
+            }
+            if (value instanceof Date) {
+                if (options.dateSchema === '$date') {
+                    if (options.dateFormat === 'JSON') {
+                        return { $date: value.toJSON() };
+                    }
+                    else { // options.dateFormat === 'UTC'
+                        return { $date: value.toUTCString() };
+                    }
+                }
+                else {
+                    if (options.dateFormat === 'JSON') {
+                        return value.toJSON();
+                    }
+                    else { // options.dateFormat === 'UTC'
+                        return value.toUTCString();
+                    }
+                }
+            }
+            return value;
+        }
+        if (replace) {
+            return function replacerHelper(key, value) {
+                const data = replace.call(this, key, value);
+                if (data !== value) {
+                    return data;
+                }
+                return replacer.call(this, key, value);
+            };
+        }
+        return replacer;
+    }
+    parse(text, reviver) {
+        if (__classPrivateFieldGet(this, _options).parse.retrocycle) {
+            return cycle.retrocycle(JSON.parse(text, this.reviver(reviver)));
+        }
+        else {
+            return JSON.parse(text, this.reviver(reviver));
+        }
+    }
+    reviver(revive) {
+        const that = this;
+        function reviver(key, value) {
+            var _a, _b, _c, _d, _e, _f;
+            if (key && value &&
+                typeof value === 'object' &&
+                value instanceof Array === false) {
+                const options = __classPrivateFieldGet(that, _options).parse;
+                if (options.setSchema === '$set' && value['$set'])
+                    return new Set(value['$set']);
+                if ((_a = options.setStringKeys) === null || _a === void 0 ? void 0 : _a.includes(key))
+                    return new Set(value);
+                if ((_b = options.setRegexKeys) === null || _b === void 0 ? void 0 : _b.test(key))
+                    return new Set(value);
+                if (options.mapSchema === '$map' && value['$map'])
+                    return new Map(value['$map']);
+                if ((_c = options.mapStringKeys) === null || _c === void 0 ? void 0 : _c.includes(key))
+                    return new Map(value);
+                if ((_d = options.mapRegexKeys) === null || _d === void 0 ? void 0 : _d.test(key))
+                    return new Map(value);
+                if (options.dateType === 'Date') {
+                    if (options.dateSchema.includes('$date') && value['$date']) {
+                        return new Date(value['$date']);
+                    }
+                    if ((_e = options.dateStringKeys) === null || _e === void 0 ? void 0 : _e.includes(key)) {
+                        return new Date(value);
+                    }
+                    if ((_f = options.dateRegexKeys) === null || _f === void 0 ? void 0 : _f.test(key)) {
+                        return new Date(value);
+                    }
+                    // YYYY-MM-DDTHH:mm:ss.sssZ | Ddd, JJ Mmm YYYY HH:mm:ss GMT
+                    if (options.dateSchema.includes('JSON&UTC') &&
+                        /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$|^[A-Z][a-z]{2}, \d{2} [A-Z][a-z]{2} \d{4} \d{2}:\d{2}:\d{2} GMT$/.test(value)) {
+                        return new Date(value);
+                    }
+                }
+                else {
+                    if (options.dateSchema.includes('$date') && value['$date']) {
+                        return value['$date'];
+                    }
+                }
+            }
+            return value;
+        }
+        if (revive) {
+            return function reviverHelper(key, value) {
+                const data = revive.call(this, key, value);
+                if (data !== value) {
+                    return data;
+                }
+                return reviver.call(this, key, value);
+            };
+        }
+        return reviver;
+    }
+}
+exports.Json = Json;
+_options = new WeakMap();
+Json._json = new Json();
